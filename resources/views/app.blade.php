5862551fb6206a1db8ae1fe770b7bde53be4920d
Daniel Tan <72296991+danieltky@users.noreply.github.com>
2024-05-31 11:35:24 +0800

    fixed font-family

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,13 +9,14 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+        
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-lexend antialiased">
+    <body class="font-sans antialiased">
         @inertia
     </body>
 </html>
