6f9b13abd13805bf49fc8e871be87c2b5e25242b
Daniel Tan <72296991+danieltky@users.noreply.github.com>
2024-07-03 13:34:35 +0800

    fix: Dropdown component
    - fixed Dropdown component's display of text and set modelValue
    - refactored and optimized some functions and classes

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -45,6 +45,13 @@ const open = ref(false);
 const selectedOption = ref("");
 const selectedValue = ref("");
 
+// Method to update the selected option
+const updateSelectedOption = (inputArray, dataValue) => {
+    let selectedText = inputArray.find(option => option.value === dataValue)?.text || props.placeholder;
+    selectedValue.value = dataValue;
+    selectedOption.value = selectedText;
+};
+
 const choose = (option) => {
     selectedOption.value = option.text;
     selectedValue.value = option.value;
@@ -58,6 +65,7 @@ const closeDropdown = (event) => {
     }
 };
 
+// v-directive for when user clicked on the outside of the declared element, it will execute the applied callback function
 const vClickOutside = {
     mounted(el, binding) {
         el.__clickOutsideHandler__ = (event) => {
@@ -73,14 +81,21 @@ const vClickOutside = {
 };
 
 onMounted(() => {
-    selectedValue.value = props.dataValue;
-    selectedOption.value = props.inputArray[props.dataValue]
-        ? props.inputArray[props.dataValue]
-        : props.placeholder;
+    updateSelectedOption(props.inputArray, props.dataValue);
     document.addEventListener("click", closeDropdown);
 });
+
 onUnmounted(() => {
     document.removeEventListener("click", closeDropdown);
+});
+
+// Watchers
+watch(() => props.inputArray, (newValue) => {
+    updateSelectedOption(newValue, props.dataValue);
+});
+
+watch(() => props.dataValue, (newValue) => {
+    updateSelectedOption(props.inputArray, newValue);
 });
 </script>
 
@@ -111,35 +126,23 @@ onUnmounted(() => {
                     @click="open = !open"
                     aria-haspopup="listbox"
                     :aria-expanded="open"
-                    aria-labelledby="assigned-to-label"
                     :disabled="props.disabled"
                     :class="[
-                        'w-full max-h-[44px] px-4 py-3 flex justify-between items-center hover:text-red-300 active:text-red-300 focus:text-red-300',
-                        'rounded-[5px] text-grey-500 active:ring-0 border border-grey-300',
-                        'hover:border-red-100 hover:shadow-[0px_0px_6.4px_0px_rgba(255,96,102,0.49)]',
-                        'active:border-red-300 active:shadow-[0px_0px_6.4px_0px_rgba(255,96,102,0.49)]',
-                        'focus:border-red-300 focus:shadow-[0px_0px_6.4px_0px_rgba(255,96,102,0.49)] focus:ring-0',
-                        {
-                            'border-grey-100': props.disabled === true,
-                            'border-grey-300': props.disabled === false,
-                            'border-red-500 focus:border-red-500 hover:border-red-500':
-                                errorMessage,
-                        },
+                        'w-full max-h-[44px] px-4 py-3 flex justify-between items-center',
+                        'rounded-[5px] text-grey-500 border',
+                        open ? 'border-red-500' : 'border-grey-300',
+                        props.disabled ? 'border-grey-100' : 'hover:border-red-100 active:border-red-300 focus:border-red-300',
+                        props.errorMessage && 'border-red-500 focus:border-red-500 hover:border-red-500'
                     ]"
                 >
                     <!-- The text to be displayed along with dropdown icons -->
                     <span
                         :class="[
                             'text-base font-normal',
-                            {
-                                'text-grey-200':
-                                    selectedOption === props.placeholder,
-                                'text-grey-700':
-                                    selectedOption !== props.placeholder,
-                            },
+                            selectedOption === props.placeholder ? 'text-grey-200' : 'text-grey-700'
                         ]"
                     >
-                        {{ selectedOption === "" ? "Select" : selectedOption }}
+                        {{ selectedOption || props.placeholder }}
                     </span>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -147,14 +150,8 @@ onUnmounted(() => {
                         height="16"
                         viewBox="0 0 16 16"
                         fill="none"
-                        :class="[
-                            open
-                                ? 'transition-all duration-200 rotate-270 transform self-center'
-                                : '',
-                            !open
-                                ? 'transition-all duration-200 rotate-180 transform self-center'
-                                : '',
-                        ]"
+                        :class="open ? 'rotate-270' : 'rotate-180'"
+                        class="transition-all duration-200 transform self-center"
                     >
                         <path
                             d="M12 10L8 6L4 10"
@@ -169,83 +166,63 @@ onUnmounted(() => {
                 <!-- The list of options for the dropdown field -->
                 <ul
                     class="z-50 absolute mt-1 p-1 bg-white rounded-[5px] border-2 border-red-50 gap-0.5 items-start flex flex-col shadow-[0px_15px_23.6px_0px_rgba(102,30,30,0.05)]"
-                    :class="[
-                        {
-                            'w-fit': grouped,
-                            'w-full': !grouped,
-                        }
-                    ]"
+                    :class="{ 'w-fit': props.grouped, 'w-full': !props.grouped }"
                     v-show="open"
                 >
                     <!-- Grouped option: group by group name and list of its options -->
-                    <div v-for="(group, index) in props.inputArray" v-if="grouped">
-                        <div
-                            :class="[
-                                'py-2 px-4 text-base text-grey-400 flex items-center bg-grey-25',
-                            ]"
-                        >
-                            <div
-                                class="flex gap-[10px] items-center justify-center"
-                            >
+                    <div v-for="(group, index) in props.inputArray" :key="index" v-if="grouped">
+                        <div class="py-2 px-4 text-base text-grey-400 flex items-center bg-grey-25">
+                            <div class="flex gap-[10px] items-center justify-center">
                                 <slot name="grouped_header">
                                     <span class="w-4 h-4 flex-shrink-0 rounded-full bg-grey-700"></span>
                                     <span class="text-base font-bold">{{ group.group_name }}</span>
                                 </slot>
                             </div>
                         </div>
-
                         <li
-                            :class="[
-                                'cursor-pointer select-none py-2 px-4 self-stretch items-center flex rounded-[5px] hover:bg-grey-50 active:bg-red-50',
-                                {
-                                    'bg-red-50': selectedOption === item.text,
-                                },
-                            ]"
                             v-for="(item, optionIndex) in group.items"
                             :key="optionIndex"
                             @click="choose(item)"
+                            :class="[
+                                'cursor-pointer select-none py-2 px-4 self-stretch items-center flex rounded-[5px] hover:bg-grey-50 active:bg-red-50',
+                                selectedOption === item.text && 'bg-red-50'
+                            ]"
                         >
                             {{ item.text }}
-
                             <!-- Optional for including an icon to the right of the text of a specified option -->
-                            <template v-if="iconOptions">
-                                <span class="absolute right-0 pr-4">
-                                    <component
-                                        :is="iconOptions[item.text]"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                /></span>
-                            </template>
-                        </li>
-                    </div>
-                    <!-- Default option: directly displays options -->
-                    <li
-                        :class="[
-                            'cursor-pointer select-none py-2 px-4 self-stretch items-center flex rounded-[5px] hover:bg-grey-50 active:bg-red-50',
-                            {
-                                'bg-red-50': selectedOption === option.text,
-                            },
-                        ]"
-                        v-else
-                        v-for="(option, ix) in props.inputArray"
-                        :key="ix"
-                        @click="choose(option)"
-                    >
-                        {{ option.text }}
-
-                        <!-- Optional for including an icon to the right of the text of a specified option -->
-                        <template v-if="iconOptions">
-                            <span class="absolute right-0 pr-4">
+                            <span class="absolute right-0 pr-4" v-if="props.iconOptions">
                                 <component
-                                    :is="iconOptions[option.text]"
+                                    :is="props.iconOptions[item.text]"
                                     width="24"
                                     height="24"
                                     viewBox="0 0 24 24"
                                     fill="none"
-                            /></span>
-                        </template>
+                                />
+                            </span>
+                        </li>
+                    </div>
+                    <!-- Default option: directly displays options -->
+                    <li
+                        v-for="(option, ix) in props.inputArray"
+                        :key="ix"
+                        @click="choose(option)"
+                        v-else
+                        :class="[
+                            'cursor-pointer select-none py-2 px-4 self-stretch items-center flex rounded-[5px] hover:bg-grey-50 active:bg-red-50',
+                            selectedOption === option.text && 'bg-red-50'
+                        ]"
+                    >
+                        {{ option.text }}
+                        <!-- Optional for including an icon to the right of the text of a specified option -->
+                        <span class="absolute right-0 pr-4" v-if="props.iconOptions">
+                            <component
+                                :is="props.iconOptions[option.text]"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                            />
+                        </span>
                     </li>
                 </ul>
             </span>
