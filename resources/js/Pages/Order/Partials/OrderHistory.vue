41cf924d5ed39205798c6c7ebbebd537df1b8a2d
danieltky <72296991+danieltky@users.noreply.github.com>
2025-06-20 13:12:13 +0800

    fix: enable print and downlaod invoice button for testing

--- resources/js/Pages/Order/Partials/OrderHistory.vue
+++ resources/js/Pages/Order/Partials/OrderHistory.vue
@@ -250,7 +250,6 @@ const printInvoiceReceipt = () => {
                     type="button"
                     variant="tertiary"
                     class="w-fit border-0 hover:bg-primary-50 !justify-start"
-                    disabled
                     @click="printInvoiceReceipt"
                 >
                     <span class="text-grey-700 font-normal">Download</span>
